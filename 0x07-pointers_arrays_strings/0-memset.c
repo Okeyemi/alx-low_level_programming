333df8e2b84e5e890f3b4532c2497b868857ae64
okeyemi <okeyemitunde@gmail.com>
2023-04-04 17:24:23 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,22 @@
 #include "main.h"
-
-char *_memset(char *s, char b, unsigned int n)
+/**
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
+ *
+ *Return: copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+	int r = 0;
+	int i = n;
 
-	for (; n > 0; i++)
+	for (; r < i; r++)
 	{
-		s[i] = b;
+		dest[r] = src[r];
 		n--;
 	}
-	return (s);
+	return (dest);
 }
+
