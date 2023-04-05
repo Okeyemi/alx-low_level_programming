d2b26cf100220c9dcd677ca8ec5d1d83bf1a56c8
okeyemi <okeyemitunde@gmail.com>
2023-04-04 18:21:35 -0700

    updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
@@ -20,4 +26,3 @@ unsigned int _strspn(char *s, char *accept)
 	}
 	return (n);
 }
-
